a4ec1547b4b281a9896b7b6b218d1c25373e0613
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2024-07-25 17:03:40 -0300

    mejora de estilso en filterProducts

--- src/components/Productos/ProductFilter.jsx
+++ src/components/Productos/ProductFilter.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './productFilter.css';
 
 const ProductFilter = ({ onFilter }) => {
   const [filters, setFilters] = useState({
@@ -29,98 +30,102 @@ const ProductFilter = ({ onFilter }) => {
   return (
     <div className="product-filter">
       <h2>Filtrar por:</h2>
-      <div>
+      <div className="filter-section">
         <label>Tipo de Bomba</label>
         <select name="tipoBomba" onChange={handleChange}>
           <option value="">Seleccionar</option>
           {/* Agregar opciones según la base de datos */}
         </select>
       </div>
-      <div>
+      <div className="filter-section">
         <label>Tipo de Aplicación</label>
         <select name="tipoAplicacion" onChange={handleChange}>
           <option value="">Seleccionar</option>
           {/* Agregar opciones según la base de datos */}
         </select>
       </div>
-      <div>
+      <div className="filter-section">
         <label>Tipo de Industria</label>
         <select name="tipoIndustria" onChange={handleChange}>
           <option value="">Seleccionar</option>
           {/* Agregar opciones según la base de datos */}
         </select>
       </div>
-      <div>
+      <div className="filter-section">
         <label>Marca de Bomba</label>
         <select name="marcaBomba" onChange={handleChange}>
           <option value="">Seleccionar</option>
           {/* Agregar opciones según la base de datos */}
         </select>
       </div>
-      <div>
+      <div className="filter-section">
         <label>Materiales</label>
         <select name="materiales" onChange={handleChange}>
           <option value="">Seleccionar</option>
           {/* Agregar opciones según la base de datos */}
         </select>
       </div>
-      <div>
+      <div className="filter-section">
         <label>Presión (m) entre</label>
-        <input
-          type="range"
-          name="presionMin"
-          min="0"
-          max="1241"
-          value={filters.presionMin}
-          onChange={(e) => handleRangeChange(e, 'presionMin')}
-        />
-        <input
-          type="number"
-          value={filters.presionMin}
-          onChange={(e) => handleRangeChange(e, 'presionMin')}
-        />
-        <input
-          type="number"
-          value={filters.presionMax}
-          onChange={(e) => handleRangeChange(e, 'presionMax')}
-        />
-        <input
-          type="range"
-          name="presionMax"
-          min="0"
-          max="1241"
-          value={filters.presionMax}
-          onChange={(e) => handleRangeChange(e, 'presionMax')}
-        />
+        <div className="range-inputs">
+          <input
+            type="range"
+            name="presionMin"
+            min="0"
+            max="1241"
+            value={filters.presionMin}
+            onChange={(e) => handleRangeChange(e, 'presionMin')}
+          />
+          <input
+            type="number"
+            value={filters.presionMin}
+            onChange={(e) => handleRangeChange(e, 'presionMin')}
+          />
+          <input
+            type="number"
+            value={filters.presionMax}
+            onChange={(e) => handleRangeChange(e, 'presionMax')}
+          />
+          <input
+            type="range"
+            name="presionMax"
+            min="0"
+            max="1241"
+            value={filters.presionMax}
+            onChange={(e) => handleRangeChange(e, 'presionMax')}
+          />
+        </div>
       </div>
-      <div>
+      <div className="filter-section">
         <label>Caudal (m3/h) entre</label>
-        <input
-          type="range"
-          name="caudalMin"
-          min="0"
-          max="9500"
-          value={filters.caudalMin}
-          onChange={(e) => handleRangeChange(e, 'caudalMin')}
-        />
-        <input
-          type="number"
-          value={filters.caudalMin}
-          onChange={(e) => handleRangeChange(e, 'caudalMin')}
-        />
-        <input
-          type="number"
-          value={filters.caudalMax}
-          onChange={(e) => handleRangeChange(e, 'caudalMax')}
-        />
-        <input
-          type="range"
-          name="caudalMax"
-          min="0"
-          max="9500"
-          value={filters.caudalMax}
-          onChange={(e) => handleRangeChange(e, 'caudalMax')}
-        />
+        <div className="range-inputs">
+          <input
+            type="range"
+            name="caudalMin"
+            min="0"
+            max="9500"
+            value={filters.caudalMin}
+            onChange={(e) => handleRangeChange(e, 'caudalMin')}
+          />
+          <input
+            type="number"
+            value={filters.caudalMin}
+            onChange={(e) => handleRangeChange(e, 'caudalMin')}
+          />
+          <input
+            type="number"
+            value={filters.caudalMax}
+            onChange={(e) => handleRangeChange(e, 'caudalMax')}
+          />
+          <input
+            type="range"
+            name="caudalMax"
+            min="0"
+            max="9500"
+            value={filters.caudalMax}
+            onChange={(e) => handleRangeChange(e, 'caudalMax')}
+          />
+        </div>
       </div>
     </div>
   );
