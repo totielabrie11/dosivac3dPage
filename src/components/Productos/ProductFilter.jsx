78709bdb66a8389c71bc71a022b788d7e45aedc4
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2024-07-29 16:50:57 -0300

    agrego filtro condicional

--- src/components/Productos/ProductFilter.jsx
+++ src/components/Productos/ProductFilter.jsx
@@ -14,6 +14,9 @@ const ProductFilter = ({ onFilter }) => {
     caudalMax: 9500,
   });
 
+  const [tipoProductoOptions, setTipoProductoOptions] = useState([]);
+  const [aplicacionOptions, setAplicacionOptions] = useState([]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFilters({ ...filters, [name]: value });
@@ -23,6 +26,37 @@ const ProductFilter = ({ onFilter }) => {
     setFilters({ ...filters, [name]: Number(e.target.value) });
   };
 
+  const fetchProductos = async () => {
+    try {
+      const response = await fetch('/api/product-descriptions');
+      const data = await response.json();
+      const tiposDeProducto = new Set();
+      const tiposDeAplicacion = new Set();
+      data.forEach(product => {
+        product.caracteristicas.forEach(caracteristica => {
+          const tipoMatch = caracteristica.match(/Tipo de Producto: (.*)/i);
+          if (tipoMatch) {
+            const tipo = tipoMatch[1].trim();
+            tiposDeProducto.add(tipo);
+          }
+          const aplicacionMatch = caracteristica.match(/Aplicación: (.*)/i);
+          if (aplicacionMatch) {
+            const aplicacion = aplicacionMatch[1].trim();
+            tiposDeAplicacion.add(aplicacion);
+          }
+        });
+      });
+      setTipoProductoOptions([...tiposDeProducto]);
+      setAplicacionOptions([...tiposDeAplicacion]);
+    } catch (error) {
+      console.error('Failed to fetch product descriptions:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductos();
+  }, []);
+
   useEffect(() => {
     onFilter(filters);
   }, [filters, onFilter]);
@@ -31,17 +65,25 @@ const ProductFilter = ({ onFilter }) => {
     <div className="product-filter">
       <h2>Filtrar por:</h2>
       <div className="filter-section">
-        <label>Tipo de Bomba</label>
+        <label>Tipo de Producto</label>
         <select name="tipoBomba" onChange={handleChange}>
           <option value="">Seleccionar</option>
-          {/* Agregar opciones según la base de datos */}
+          {tipoProductoOptions.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
       </div>
       <div className="filter-section">
         <label>Tipo de Aplicación</label>
         <select name="tipoAplicacion" onChange={handleChange}>
           <option value="">Seleccionar</option>
-          {/* Agregar opciones según la base de datos */}
+          {aplicacionOptions.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
       </div>
       <div className="filter-section">
